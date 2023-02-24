4b8e1e9631d5f2e32594ce5ed134da485ea5f8c8
AminSiddika <118524853+AminSiddika@users.noreply.github.com>
2023-02-24 18:29:46 +0600

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-echo "Cloning Repo..."
-git clone https://github.com/aceknox/Url-Uploader-Bot-V4 
-cd /Url-Uploader-Bot-V4 
-pip3 install -U -r requirements.txt
-echo "Starting Bot.."
-python3 bot.py
